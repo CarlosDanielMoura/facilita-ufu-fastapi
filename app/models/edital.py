3317d227b511765b311ad77b2fbffe18208e85ba
Carlos Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2024-02-29 07:43:57 -0300

    ✨ feat(app/models): added models edital
    - adicionado modelo edital

---
+++ app/models/edital.py
@@ -0,0 +1,14 @@
+from sqlalchemy import TIMESTAMP, Column, Integer, String, text
+from app.database.connection import Base
+
+class Edital(Base):
+    __tablename__ = "edital.py"
+    cod_edital = Column(Integer, primary_key=True, nullable=False)
+    org_resp = Column(String, nullable=False)
+    titulo = Column(String, nullable=False)
+    tipo = Column(String, nullable=False)
+    dt_publicacao = Column(String, nullable=False)
+    link = Column(String, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
+    )
