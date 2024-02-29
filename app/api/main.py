555c0649a524ca8e5b5f31e878e389f19a4d64c0
Carlos Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2024-02-29 07:45:14 -0300

    ✨ feat(app/api): added routes edital
    - adicionado rota do edital em api

--- app/api/main.py
+++ app/api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends
 from app.database.connection import get_db
 from sqlalchemy.orm import Session
 from app.controller.horario_onibus import get_horario_onibus
+from app.controller.edital import get_edital
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ app = FastAPI()
 @app.get("/horario_onibus")
 def get_horario_onibus_all(db: Session = Depends(get_db)):
     return get_horario_onibus(db)
+
+
+@app.get("/edital")
+def get_edital_all(db: Session = Depends(get_db())):
+    return get_edital(db)
