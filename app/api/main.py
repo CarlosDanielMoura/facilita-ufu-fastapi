50eb91bf4e1c83c4e3e64489c2b6c8536c8b98bb
Carlos Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2024-02-28 21:40:55 -0300

    ✨ feat(app/api): added route
    adicionado rota  horario_onibus

--- app/api/main.py
+++ app/api/main.py
@@ -1,10 +1,11 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from app.database.connection import get_db
+from sqlalchemy.orm import Session
+from app.controller.horario_onibus import get_horario_onibus
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/horario_onibus")
+def get_horario_onibus_all(db: Session = Depends(get_db)):
+    return get_horario_onibus(db)
