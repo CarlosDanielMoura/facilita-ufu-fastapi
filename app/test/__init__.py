744c261da0b5fc9badcc7aae205871dabd7aec81
Carlos Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2024-03-07 20:29:58 -0300

    fix: remove test/__init__.py

--- app/test/__init__.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-# Suprimir o aviso específico durante os testes
-@pytest.fixture(autouse=True)
-def suppress_warnings():
-    with pytest.warns(None) as warnings:
-        yield
-
-# Outras configurações
