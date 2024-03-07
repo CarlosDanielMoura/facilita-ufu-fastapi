8ec54d5d58e71222a32ae0adcc9461dd72e7fa38
Carlos Daniel <58231139+CarlosDanielMoura@users.noreply.github.com>
2024-03-07 20:04:55 -0300

    🐞 fix(app): adjusted files

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,13 +1,24 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+import os
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+Base = declarative_base()
 
-DATABASE_URL = "postgresql://user:password@localhost/database"
+
+load_dotenv()
+
+DB_USER = os.getenv("DATABASE_USERNAME")
+DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DB_HOSTNAME = os.getenv("DATABASE_HOSTNAME")
+DB_DATABASE = os.getenv("DATABASE_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}/{DB_DATABASE}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
 
 
 def get_db():
