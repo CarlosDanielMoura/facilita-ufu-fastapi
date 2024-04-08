3c819e52141b00c6d14dd33f8f8ef9c8ae4fbaa0
Bianca Ataide <ataide.bianca@proton.me>
2024-04-08 17:58:54 -0300

    Atualização editais

--- app/webscraping/editais/editais_ufu.py
+++ app/webscraping/editais/editais_ufu.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from sqlalchemy import create_engine
+import json
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
 
 url = 'http://www.editais.ufu.br/discente'
 base_url = 'http://www.editais.ufu.br/discente?page='
@@ -9,6 +15,7 @@ try:
 except TypeError as e: 
     print(str(e))
 
+dados = []
 
 orgao = html[0]['Orgão Resp.']
 titulo = html[0]['Título']
@@ -36,6 +43,19 @@ if(html2.status_code==200):
          l="http://www.editais.ufu.br"+links.get("href")
          link.append(l)
    
+for i in  range(orgao.count()):
+    dados.append(
+        {
+            'orgao_responsavel': orgao[i],
+            'titulo': titulo[i],
+            'link': link[i],
+            'tipo':  tipo[i],
+            'data_publicacao': data[i],
+            'resultado': resultado[i]
+        }
+    )
+
+
 
 
 df = pd.DataFrame({'Orgao Responsável':orgao, 'Título':titulo, 'Link': link, 'Tipo':tipo, 'Data de Publicação':data, 'Resultado': resultado})
@@ -78,16 +98,103 @@ for pagina in range(1, 59):
         df_2 = pd.DataFrame({'Orgao Responsável':org, 'Título':tit, 'Link': link2, 'Tipo':tip, 'Data de Publicação':dat, 'Resultado': res})
         df = pd.concat([df, df_2], ignore_index=True)
 
-df_remove = df.loc[(df['Resultado'] == 'Publicado')]
+        
+    for i in  range(org.count()):
+        dados.append(
+            {
+                'orgao_responsavel': org[i],
+                'titulo': tit[i],
+                'link': link2[i],
+                'tipo':  tip[i],
+                'data_publicacao': dat[i],
+                'resultado': res[i]
+            }
+        )
+        
+    data = df['Data de Publicação'].iloc[-1]
+    d,y,a,b = data.split()
+    # print(y)
+    y = datetime.strptime(y, '%Y-%m-%d')
+    current_date = datetime.today()
+    past_date = current_date + relativedelta(years=-2)
+    if(y<past_date):
+        break
 
-df = df.drop(df_remove.index)
 
-df.drop(columns=['Resultado'])
 
-df = df.reset_index(drop='true')
+# df_remove = df.loc[(df['Resultado'] == 'Publicado')]
 
-df.to_csv('editais_ufu.csv', index=False, encoding='utf-8')
-df.to_excel("editais.xlsx")
+# df = df.drop(df_remove.index)
+
+# df.drop(columns=['Resultado'], axis=1)
+
+# df = df.reset_index(drop='true')
+
+# df.to_csv('edital.csv', index=False, encoding='utf-8-sig')
+# df.to_excel("editais.xlsx")
+
+# df.to_json('editais.json', orient='records', indent=4)
+# with open('editais.json', 'r', encoding='utf-8') as file:
+#     dados_json = json.load(file)
+
+
+
+test = pd.DataFrame(dados)
+# print(type(test))
+# print(test)
+
+test_remove = test.loc[(df['Resultado'] == 'Publicado')]
+
+test = test.drop(test_remove.index)
+
+test = test.drop(columns=['resultado'], axis=1)
+
+test = test.reset_index(drop='true')
+
+test.to_csv('edital_ufu.csv', index=False, encoding='utf-8-sig')
+
+
+#######################################################################################################
+
+## Conexão banco
+
+import psycopg2
+  
+# connection establishment
+conn = psycopg2.connect(
+    user="postgres",
+    password="123",
+    host="localhost",
+    port="5432",
+    dbname="pds2"
+)
+  
+conn.autocommit = True
+  
+# Creating a cursor object
+cursor = conn.cursor()
+  
+# cursor.execute ('''CREATE TABLE IF NOT EXISTS EDITAL
+#                     (
+#                         COD_EDITAL SERIAL,
+#                         ORG_RESP character text COLLATE pg_catalog."default" NOT NULL,
+#                         TITULO character text COLLATE pg_catalog."default" NOT NULL,
+#                         LINK character varying(100) COLLATE pg_catalog."default" NOT NULL,
+#                         TIPO character varying(50) COLLATE pg_catalog."default" NOT NULL,
+#                         DT_PUBLICACAO character varying(50) COLLATE pg_catalog."default" NOT NULL,
+#                         CREATED_AT timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+#                         CONSTRAINT edital_pkey PRIMARY KEY (COD_EDITAL)
+#                     )''')
+# query to import data from given csv
+with open('edital_ufu.csv', 'r', encoding='utf-8') as f:
+    cursor.copy_expert(sql="COPY EDITAL(ORG_RESP,TITULO, LINK, TIPO, DT_PUBLICACAO) FROM STDIN WITH CSV HEADER", file=f)
+
+# Display the table
+cursor.execute('SELECT * FROM edital')
+print(cursor.fetchall())
+
+# Closing the connection
+conn.close()
 
 
 
